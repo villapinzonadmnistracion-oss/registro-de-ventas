7cc4690803286b04bb96a3c0ef11868c78068951
villapinzonadmnistracion-oss <villapinzonadmnistracion@gmail.com>
2025-10-09 12:01:17 -0300

    validacion

--- main.js
+++ main.js
@@ -41,7 +41,6 @@ function formatRut(value) {
   // Separar dígito verificador
   const dv = value.slice(-1);
   let rut = value.slice(0, -1);
-    let isRutValid = false;
   
   // Formatear con puntos
   let formatted = '';
